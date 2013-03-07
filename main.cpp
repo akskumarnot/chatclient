282f518a83578b8348b4160fc723358c6b702252
aksnot <nautigitakshay@gmail.com>
2013-03-07 11:56:48 +0530

    just for testing through router

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include<QApplication>
 #include"ui.h"
+#include"server.h"
 
 int main(int argc,char * argv[])
 {
 QApplication app(argc,argv);
+server *s=new server();
+s->start();
 ui *u=new ui();
+QObject::connect(s,SIGNAL(sendtomain(QString)),u,SLOT(rAtMain(QString)));
 u->show();
 return app.exec();
 }
